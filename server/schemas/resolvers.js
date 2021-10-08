7f22c381bc97416e12e6cb2a0191dc6c7a8847cd
ywkuo227 <ywkuo@outlook.com>
2021-10-07 22:25:06 -0700

    Oct 7th code update #1: - Implemented Server-side TypeRefs and Client-side Queries and Mutations. - Working on Server-side Resolvers.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,56 @@
+const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
-
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('savedBooks');
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
     },
     Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { BookInput }, context) => {
+            if (context.user) {
+              const savBook = await Book.create({
+                ...BookInput
+              });
+      
+              await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: Book.bookId } }
+              );
+      
+              return user;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
 
     },
 };
